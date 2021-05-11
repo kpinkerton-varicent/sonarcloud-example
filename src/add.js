108922117b03fff4fbf2c7ba409c4d5bb6c5d5a4
kpinkerton-varicent <kpinkerton@varicent.com>
2021-05-11 07:43:33 -0400

    decrease coverage

---
+++ src/add.js
@@ -0,0 +1,20 @@
+// all of the following functions
+// will not be tested!
+
+const add = (x, y) => {
+  return x + y;
+}
+
+const subtract = (x, y) => {
+  return x - y;
+}
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+const divide = (x, y) => {
+  return x / y;
+}
+
+
